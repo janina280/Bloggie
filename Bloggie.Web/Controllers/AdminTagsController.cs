2c6b30aacc776eea3bc4a86e0abe9089b7ce9bc7
janina280 <coceijanina@yahoo.com>
2024-06-06 16:42:51 +0300

    Add Tag Request

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers;
 
 public class AdminTagsController : Controller
 {
+    private readonly BloggieDbContext bloggiedbContext;
+    public AdminTagsController(BloggieDbContext bloggieDbContext)
+    {
+       this.bloggiedbContext=bloggieDbContext;
+    }
+
     [HttpGet]
     public IActionResult Add()
     {
         return View();
+    }
+    [HttpPost]
+    [ActionName("Add")]
+    public IActionResult Add(AddTagRequest addTagRequest)
+    {
+        //Mapping AddTagRequest to Tag domain model
+        var tag = new Tag
+        {
+            Name = addTagRequest.Name,
+            DisplayName = addTagRequest.DisplayName
+        };
+        bloggiedbContext.Tags.Add(tag);
+        bloggiedbContext.SaveChanges();
+
+        return View("Add");
     }
 }
