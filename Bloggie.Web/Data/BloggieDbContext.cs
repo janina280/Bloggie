7218c9ef41440adfeb4a7f13b226dd67d67be3df
janina280 <coceijanina@yahoo.com>
2024-06-06 15:10:06 +0300

    Add Admin Tags Controller

--- Bloggie.Web/Data/BloggieDbContext.cs
+++ Bloggie.Web/Data/BloggieDbContext.cs
@@ -1,13 +1,12 @@
 ﻿using Bloggie.Web.Models.Domain;
 using Microsoft.EntityFrameworkCore;
 
-namespace Bloggie.Web.Data
-{
-    public class BloggieDbContext:DbContext
-    { 
-        public BloggieDbContext(DbContextOptions options):base(options) { }
+namespace Bloggie.Web.Data;
 
-        public DbSet<BlogPost> BlogPosts { get; set; }
-        public DbSet<Tag> Tags { get; set; }
-    }
+public class BloggieDbContext:DbContext
+{ 
+    public BloggieDbContext(DbContextOptions options):base(options) { }
+
+    public DbSet<BlogPost> BlogPosts { get; set; }
+    public DbSet<Tag> Tags { get; set; }
 }
--- Bloggie.Web/Models/Domain/Tag.cs
+++ Bloggie.Web/Models/Domain/Tag.cs
@@ -1,10 +1,9 @@
-﻿namespace Bloggie.Web.Models.Domain
+﻿namespace Bloggie.Web.Models.Domain;
+
+public class Tag
 {
-    public class Tag
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string DisplayName { get; set; }
-        public ICollection<BlogPost> BlogPosts { get; set; }
-    }
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string DisplayName { get; set; }
+    public ICollection<BlogPost> BlogPosts { get; set; }
 }
