99b190e665aae2e853669195551066ed0490ca47
janina280 <coceijanina@yahoo.com>
2024-07-04 00:18:00 +0300

    Edit Tag Functionality

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -2,6 +2,7 @@
 using Bloggie.Web.Models.Domain;
 using Bloggie.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bloggie.Web.Controllers;
 
@@ -43,4 +44,48 @@ public class AdminTagsController : Controller
         var tags=bloggieDbContext.Tags.ToList();
         return View(tags);
     }
+
+    [HttpGet]
+    public IActionResult Edit(Guid id)
+    {
+        //1st  method
+     //var tag=   bloggieDbContext.Tags.Find(id);
+
+     //2nd method
+     var tag = bloggieDbContext.Tags.FirstOrDefault(x => x.Id == id);
+    if (tag != null)
+    {
+        var editTagRequest = new EditTagRequest()
+        {
+            Id = tag.Id,
+            Name = tag.Name,
+            DisplayName = tag.DisplayName
+        };
+        return View(editTagRequest);
+    }
+
+    return View(null);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(EditTagRequest editTagRequest)
+    {
+        var tag = new Tag
+        {
+            Id = editTagRequest.Id,
+            Name = editTagRequest.Name,
+            DisplayName = editTagRequest.DisplayName
+        };
+       var existingTag= bloggieDbContext.Tags.Find(tag.Id);
+       if (existingTag != null)
+       {
+            existingTag.Name=tag.Name;
+            existingTag.DisplayName=tag.DisplayName;
+            bloggieDbContext.SaveChanges();
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+        }
+
+       return RedirectToAction("Edit", new {id= editTagRequest.Id});
+    }
+
 }
